3afe8a7b4e83bcc23fb532b6c5e4a0e8f2e81942
atoolate <alexdelaet00@gmail.com>
2024-11-15 17:26:39 +0100

    feat: add admin page url security check and logout functionality

--- admin.php
+++ admin.php
@@ -1,6 +1,14 @@
 <?php 
     namespace Alex\Eindwerk;
     include_once(__DIR__ . '/vendor/autoload.php');
+
+    session_start();
+    // if is Admin is false, redirect to index.php
+    if (!isset($_SESSION['admin']) || $_SESSION['admin'] === false) {
+        header('Location: index.php');
+        exit;
+    }    
+
     
 
 ?><!DOCTYPE html>
@@ -9,8 +17,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <a href="logout.php">Logout</a>
 </body>
 </html>
