0fe86a672af4ab87c0e02fdc7a267abe8cd5f6ca
atoolate <alexdelaet00@gmail.com>
2024-12-11 15:01:16 +0100

    fix: updated popup script for fluency

--- js/signupPopup.js
+++ js/signupPopup.js
@@ -1,16 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-        const accountCreated = document.getElementById("account-created");
-        const form = document.getElementById("multi-step-form");
-        const popup = document.getElementById("popup");
+    const accountCreated = document.getElementById("account-created");
+    const loginPage = document.querySelector(".login");
+    const popup = document.getElementById("popup");
 
-        if (accountCreated && accountCreated.value === "true") {
-            form.style.display = "none"; // Hide the for
-            popup.style.display = "block"; // Show the popup
-            setTimeout(() => {
-            }, 1000);
-            setTimeout(() => {
-                popup.style.display = "none"; // Hide the popup after 3 seconds
-                window.location.href = "login.php"; // Redirect to login page
-            }, 4000); // 3 seconds delay
-        }
-    });
+    if (accountCreated && accountCreated.value === "true") {
+        loginPage.style.display = "none"; // Hide the entire login page
+        popup.style.display = "block"; // Show the popup
+        setTimeout(() => {
+            window.location.href = "login.php"; // Redirect to login page after 3 seconds
+        }, 3000); // 3 seconds delay
+    }
+});
